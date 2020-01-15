7012d980add638d82dd97e0f6eb540682311041e
Mickkay22 <Mikayla.kosmala@gmail.com>
2020-01-14 21:16:52 -0800

    Finalized

--- static/js/app.js
+++ static/js/app.js
@@ -11,7 +11,8 @@ timeline: false
 });
 
 function buildGlobe(data,countriesSel, yearSel) {
-    var countriesBorders = "http://raw.githubusercontent.com/sebringjulia/UCI_project2/master/geojson/countries.geo.json";
+    // viewer.dataSources.removeAll()
+    countriesBorders = "http://raw.githubusercontent.com/sebringjulia/UCI_project2/master/geojson/countries.geo.json";
     // Create a new GeoJSON data source and add it to the list.
     var dataSource = new Cesium.GeoJsonDataSource();
     viewer.dataSources.add(dataSource);
@@ -21,20 +22,46 @@ function buildGlobe(data,countriesSel, yearSel) {
         }).then(function() {
         // Get the array of entities
         var entities = dataSource.entities.values;
+
+        var colorHash = {};
+
         for (var i = 0; i < entities.length; i++) {
-            //For each entity
-            var entity = entities[i];
-            var name = entity.name;
-            // update selected country
-            if (name == countriesSel) {
-                // var data = d3.json(`/Countryfilter/${countriesSel}`).then((data) => {
-                //     var yearHappiness = data[0]["Happiness Score"];
-                //
-                // });
-                console.log(`You selected ${countriesSel} for ${yearSel}`);
-                entity.polygon.extrudedHeight = 1000000;
-                entity.polygon.fill = true;
+            for (var j= 0; j < data.length; j++) {
+                //For each entity
+                var entity = entities[i];
+                var name = entity.name;
+                // update selected country
+                if (name == data[j]['Country']) {
+                    // var data = d3.json(`/Countryfilter/${countriesSel}`).then((data) => {
+                    //     var yearHappiness = data[0]["Happiness Score"];
+                    //
+                    // });
+                    entity.polygon.fill = true;
+
+                    if (data[j]['Happiness Score']<2){
+                        entity.polygon.material = Cesium.Color.DARKGREY
+                    } 
+                    else if (data[j]['Happiness Score']>2 && data[j]['Happiness Score']<4){
+                        entity.polygon.material = Cesium.Color.DARKORANGE
+                    }
+                    else if (data[j]['Happiness Score']>4 && data[j]['Happiness Score']<6){
+                        entity.polygon.material = Cesium.Color.GOLDROD
+                    }
+                    else if (data[j]['Happiness Score']>6 && data[j]['Happiness Score']<8){
+                        entity.polygon.material = Cesium.Color.GOLD
+                    }
+                    else if (data[j]['Happiness Score']>8 && data[j]['Happiness Score']<10){
+                        entity.polygon.material = Cesium.Color.YELLOW
+                    }
+                    else {
+
+                    };
+
+                    console.log(`You selected ${countriesSel} for ${yearSel}`);
+                    entity.polygon.extrudedHeight = data[j]['Happiness Score']*100000;
+
             }
+        }
         }
     }).otherwise(function(error) {
         //Display any errors encountered while loading.
@@ -100,7 +127,7 @@ function optionChanged(new_value,category) {
         var country = document.getElementById("selDatasetThree").value;
         var year = new_value;
     }
-    buildVisuals(year,country);
+    buildVisuals(year,country)
 }
 
 init();
